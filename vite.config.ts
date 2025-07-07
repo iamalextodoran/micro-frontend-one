b89e5072621b7d993bd76772a3dd364db9aa57fc
Alex Todoran <alex.todoran@luminos.software>
2025-07-07 14:45:27 +0300

    Two

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "micro_frontend_one",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx", // or wherever your component lives
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
